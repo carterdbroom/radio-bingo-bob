fc5462af2ad20c9f7845ed0ac7737df1e2eb526f
SWEEPC2 <carterdbroom@gmail.com>
2024-06-19 14:55:23 -0400

    Created BingoPage class and added comments.

--- classes.py
+++ classes.py
@@ -1,4 +1,12 @@
 #############################################################
+# The BingoPage class is used to represent the page as data. 
+# The page has its own id, and has 6 cards that belong to it.
+#############################################################
+class BingoPage: 
+    def __init__(self, id: int) -> None:
+        self.pageID = id
+
+#############################################################
 # The BingoCard class is used to represent a card as data. 
 # Each card belongs to a page, which has an ID. There are 6
 # cards per page, and each card has a coordinate (1-6). Each 
@@ -6,9 +14,9 @@
 # one line completed, at least two lines completed, or the 
 # whole card completed.
 #############################################################
-class BingoCard:
-    def __init__(self, id: int, coordinate: int, single: bool, double: bool, full: bool):
-        self.pageID = id
+class BingoCard(BingoPage):
+    def __init__(self, id: int, coordinate: int, single: bool, double: bool, full: bool) -> None:
+        super().__init__(id)
         self.pageCoordinate = coordinate
         self.hasSingleWon = single
         self.hasDoubleWon = double
