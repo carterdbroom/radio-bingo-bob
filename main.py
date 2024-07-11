e18d347447b804033333fde53edb11021d08179b
Carter Broom <carterdbroom@gmail.com>
2024-07-10 22:17:08 -0400

    Worked on how reading from the text file will work.

---
+++ main.py
@@ -0,0 +1,9 @@
+
+listOfCards = ["                 bro       \n", 'sldkfjsdlkfjskldfj    \n', "wassup cuh\n"]
+
+strippedList = []
+
+for number in listOfCards:
+    strippedList.append(number.strip())
+
+print(strippedList)
